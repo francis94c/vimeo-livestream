3dfcc2c0b12226fb1cb65ec2684b33778e8fa5a7
Francis Ilechukwu <francis94c@gmail.com>
2020-07-21 22:45:31 +0100

    Use the Picture Object.

--- src/LiveStream/Resources/Account.php
+++ src/LiveStream/Resources/Account.php
@@ -4,4 +4,12 @@ namespace LiveStream\Resources;
 
 class Account extends Resource
 {
+    public static function fromObject(object $object): Resource
+    {
+        $instance = parent::fromObject($object);
+
+        $instance->picture = Picture::fromObject($instance->picture);
+        
+        return $instance;
+    }
 }
