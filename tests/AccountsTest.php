e78abc279d665d4a42e8a30023e0c2fa4d0b8031
Francis Ilechukwu <francis94c@gmail.com>
2020-07-21 22:51:29 +0100

    More Assertions.

--- tests/AccountsTest.php
+++ tests/AccountsTest.php
@@ -5,8 +5,9 @@ declare(strict_types=1);
 namespace Tests;
 
 use LiveStream\LiveStream;
-
 use PHPUnit\Framework\TestCase;
+use LiveStream\Resources\Account;
+use LiveStream\Resources\Picture;
 
 
 final class AccountsTest extends TestCase
@@ -24,10 +25,16 @@ final class AccountsTest extends TestCase
 
         $this->assertCount(2, $accounts);
 
+        $this->assertInstanceOf(Account::class, $accounts[0]);
+        $this->assertInstanceOf(Account::class, $accounts[1]);
+
         $this->assertEquals(18855759, $accounts[0]->id);
 
         $this->assertEquals(18855760, $accounts[1]->id);
         $this->assertEquals('Apitest2', $accounts[1]->fullName);
+
+        $this->assertInstanceOf(Picture::class, $accounts[0]->picture);
+        $this->assertInstanceOf(Picture::class, $accounts[1]->picture);
     }
 
     /**
