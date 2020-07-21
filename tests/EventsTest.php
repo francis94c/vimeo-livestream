048c430719316a470b9b2586f9ec5b9ff9311e94
Francis Ilechukwu <francis94c@gmail.com>
2020-07-21 18:24:55 +0100

    Tests.

--- tests/EventsTest.php
+++ tests/EventsTest.php
@@ -15,7 +15,7 @@ class EventsTest extends TestCase
      *
      * @return void
      */
-    public function testCanSetFullName():void
+    public function testCanSetFullName(): void
     {
         $event = new Event("Full Name");
 
@@ -28,7 +28,7 @@ class EventsTest extends TestCase
      *
      * @return void
      */
-    public function testCanSetShortName():void
+    public function testCanSetShortName(): void
     {
         $event = new Event("Full Name");
 
@@ -36,5 +36,55 @@ class EventsTest extends TestCase
 
         $this->assertEquals('Short Name', $event->getShortName());
         $this->assertEquals('Short Name', $event->shortName);
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function testCanSetStartTime(): void
+    {
+        $event = new Event("Full Name");
+
+        $event->setStartTime('2020-07-21 17:59:24');
+
+        $this->assertEquals('2020-07-21T17:59:24+00:00', $event->getStartTime());
+        $this->assertEquals('2020-07-21T17:59:24+00:00', $event->startTime);
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function testCanSetEndTime(): void
+    {
+        $event = new Event("Full Name");
+
+        $event->setEndTime('2020-07-21 17:59:24');
+
+        $this->assertEquals('2020-07-21T17:59:24+00:00', $event->getEndTime());
+        $this->assertEquals('2020-07-21T17:59:24+00:00', $event->endTime);
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function canSetDraft(): void
+    {
+        $event = new Event("Full Name");
+
+        $event->setIsDraft(true);
+
+        $this->assertTrue($event->isDraft());
+        $this->assertTrue($event->draft);
+
+        $event->setIsDraft(false);
+
+        $this->assertFalse($event->isDraft());
+        $this->assertFalse($event->draft);
     }
 }
